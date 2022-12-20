651efcecbb776c40bd503b6edb8eab7056a4905b
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-20 00:48:46 +0000

    feat: optimized image file size

--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -8,7 +8,8 @@ defineProps<{
 
 <template>
     <div class="p-4 flex flex-col items-start min-h-[250px]">
-        <NuxtImg provider="cloudinary" :src="img" class="w-72 h-80 object-cover" :alt="name" />
+        <NuxtImg provider="cloudinary" format="webp" width="288px" height="320px" quality="70" fit="cover" :src="img"
+            class="w-72 h-80 object-cover" :alt="name" />
         <div class="py-2 text-center w-full">
             <h3 class="text-xl font-semibold">{{ name }}</h3>
             <p>{{ position }}</p>
